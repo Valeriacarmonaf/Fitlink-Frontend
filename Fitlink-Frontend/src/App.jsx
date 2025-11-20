2b95f0ae995e3db947d14d3c69ce2dfa217c0e55
Luigi Lauricella <l.lauricella@correo.unimet.edu.ve>
2025-11-19 21:55:56 -0400

    Fix: Usuarios sugeridos click

--- Fitlink-Frontend/src/App.jsx
+++ Fitlink-Frontend/src/App.jsx
@@ -112,7 +112,7 @@ function App() {
 
           <Route 
             path="/notificaciones"
-            element={session ? <Notificaciones /> : <Navigate to="/login" replace />}
+            element={session ? <Notificaciones session={session} /> : <Navigate to="/login" replace />}
           />
 
           {/* ❌ Página No Encontrada */}
